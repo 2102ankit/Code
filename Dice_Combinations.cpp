fa42d800df7ed73c00d3bdc5980abfd64dd0074e
2102ankit <105378102+2102ankit@users.noreply.github.com>
2023-06-17 02:01:35 +0530

    Template update

---
+++ Dice_Combinations.cpp
@@ -0,0 +1,270 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vi;
+typedef vector<pll> vpll;
+typedef unordered_map<ll, ll> umap;
+#define F first
+#define S second
+#define pb push_back
+#define ar array
+#define ll long long
+#define ld long double
+#define sz(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define endl "\n"
+#define forr(n) for (int i = 0; i < n; i++)
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+#ifdef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x) ;
+#endif
+
+// using ll = long long;
+using ull = unsigned long long;
+using lld = long double;
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+// void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b)
+{
+    if (b > a)
+    {
+        return gcd(b, a);
+    }
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+ll expo(ll a, ll b, ll mod)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b = b >> 1;
+    }
+    return res;
+}
+void extendgcd(ll a, ll b, ll *v)
+{
+    if (b == 0)
+    {
+        v[0] = 1;
+        v[1] = 0;
+        v[2] = a;
+        return;
+    }
+    extendgcd(b, a % b, v);
+    ll x = v[1];
+    v[1] = v[0] - v[1] * (a / b);
+    v[0] = x;
+    return;
+} // pass an arry of size1 3
+ll mminv(ll a, ll b)
+{
+    ll arr[3];
+    extendgcd(a, b, arr);
+    return arr[0];
+} // for non prime b
+ll mminvprime(ll a, ll b) { return expo(a, b - 2, b); }
+bool revsort(ll a, ll b) { return a > b; }
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact)
+{
+    ll val1 = fact[n];
+    ll val2 = ifact[n - r];
+    ll val3 = ifact[r];
+    return (((val1 * val2) % m) * val3) % m;
+}
+void google(int t) { cout << "Case " << t << ": "; }
+vector<ll> sieve(int n)
+{
+    int *arr = new int[n + 1]();
+    vector<ll> vect;
+    for (int i = 2; i <= n; i++)
+        if (arr[i] == 0)
+        {
+            vect.push_back(i);
+            for (int j = 2 * i; j <= n; j += i)
+                arr[j] = 1;
+        }
+    return vect;
+}
+ll mod_add(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+ll mod_mul(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+ll mod_sub(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+ll mod_div(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (mod_mul(a, mminvprime(b, m), m) + m) % m;
+} // only for prime m
+ll phin(ll n)
+{
+    ll number = n;
+    if (n % 2 == 0)
+    {
+        number /= 2;
+        while (n % 2 == 0)
+            n /= 2;
+    }
+    for (ll i = 3; i <= sqrt(n); i += 2)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            number = (number / i * (i - 1));
+        }
+    }
+    if (n > 1)
+        number = (number / n * (n - 1));
+    return number;
+} // O(sqrt(N))
+// ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);}
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi dp(n + 1, 1);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        ll a = 0;
+        for (int j = i; j > 0; j--)
+        {
+
+            a += dp[j];
+        }
+        dp[i] = a;
+    }
+    forr(n+1) cout << dp[i] << " ";
+    // cout << dp[n] % MOD;
+}
+
+int main()
+{
+    fastio();
+#ifdef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+    int tc = 1;
+    // cin >> tc;
+
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
